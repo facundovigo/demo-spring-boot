1182eb71c632ff17b7fdb193383c09f05d6084f1
facundo <facundovigo@gmail.com>
2019-08-27 16:27:27 -0300

    mi primer commit

---
+++ src/main/java/ar/com/bbva/api/Demo.java
@@ -0,0 +1,18 @@
+package ar.com.bbva.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/demo")
+public class Demo {
+
+    @RequestMapping(value="/saludo/{nombre}",method = RequestMethod.GET)
+    public Persona holaMundo(@PathVariable String nombre, @RequestParam String apellido){
+
+
+        //return "Hola " + nombre + " " + apellido;
+
+        return  new Persona(nombre, apellido);
+    }
+
+}
